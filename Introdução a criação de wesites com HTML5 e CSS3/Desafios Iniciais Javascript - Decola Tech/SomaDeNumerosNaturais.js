46c5287922b4cb038363809472117921f5820649
Matheus Santiago <42156446+Matheussantiagou@users.noreply.github.com>
2022-05-15 23:21:02 -0300

    Delete Introdução a criação de wesites com HTML5 e CSS3/Desafios Iniciais Javascript - Decola Tech directory

--- Introdução a criação de wesites com HTML5 e CSS3/Desafios Iniciais Javascript - Decola Tech/SomaDeNumerosNaturais.js
+++
@@ -1,6 +0,0 @@
-const a = parseInt(gets())
-const b = parseInt(gets())
-
-const total = (a+b) * (b-a+1)/2
-
-print(total)
--- Introdução a criação de wesites com HTML5 e CSS3/Desafios Iniciais Javascript - Decola Tech/deuALoucaNoGerente.js
+++
@@ -1,8 +0,0 @@
-let input = gets();
-let desconto = parseInt(input):
-
-input = gets();
-let precoAntigo = parseInt(input);
-let precoNovo = precoAntigo - (input * (desconto/100));
-
-print(precoNovo);
