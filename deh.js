9eb217a1dac871027bd070183a9e2c9aba8a0c87
Matheus Santiago <42156446+Matheussantiagou@users.noreply.github.com>
2022-05-17 01:42:59 +0000

    New files

---
+++ deh.js
@@ -0,0 +1,31 @@
+function validaArr(arr, num){
+  try{
+    if(!arr && !num) throw new ReferenceError("Envie os parametros");
+    if(typeof arr !== Object) throw new TypeError("O array precisa ser tipo object");
+    if(typeof arr !== number) throw new TypeError("O array precisa ser tipo numero");
+    if(arr.length !== num) throw new RangeError("Tamanho invalido");
+
+    return arr
+  }catch(e){
+    if(e instanceof ReferenceError){
+      console.log("Este erro é um referenceError!")
+      console.log(e.message)
+      console.log(e.stack)
+    }else if(e instanceof TypeError){
+      
+      console.log("Este erro é um TypeError!")
+      console.log(e.message)
+      console.log(e.stack)
+    }else if(e instanceof RangeError){
+      
+      console.log("Este erro é um RangeError!")
+      console.log(e.message)
+      console.log(e.stack)
+    }else{
+        console.log("Erro n esperado:" + e)
+    }
+  
+  }
+}  
+
+console.log(validaArr())
