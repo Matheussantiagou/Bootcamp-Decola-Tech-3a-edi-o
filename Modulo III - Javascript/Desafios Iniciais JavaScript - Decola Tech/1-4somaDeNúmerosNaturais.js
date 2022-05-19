bd2ac4f4afeac2529d1f3f1a6e5918039187d6ef
Matheus Santiago <42156446+Matheussantiagou@users.noreply.github.com>
2022-05-19 06:18:21 +0000

    up desafios iniciais

---
+++ Modulo III - Javascript/Desafios Iniciais JavaScript - Decola Tech/1-4somaDeNúmerosNaturais.js
@@ -0,0 +1,6 @@
+const a = parseInt(gets());
+const b = parseInt(gets());
+
+const total = (a + b) * (b - a + 1) / 2;
+
+print(total);
---
+++ Modulo III - Javascript/Desafios Iniciais JavaScript - Decola Tech/3-4areaDoCirculo.js
@@ -0,0 +1,4 @@
+const TT = 3.14159;
+let raio = parseFloat(gets());
+let area =    TT         *  Math.pow(raio, 2)
+print("A=" + area.toFixed(4));
